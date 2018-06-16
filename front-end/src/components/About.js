f65a5f56e6166081f31787ddef2de6576080ca8d
guyit <guyit@github.com>
2018-06-16 09:22:28 -0700

    Created components and changed front-end app.js

---
+++ front-end/src/components/About.js
@@ -0,0 +1,14 @@
+// About.js
+
+import React, { Component } from 'react';
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <h2>About</h2>
+                <p>Fauna is...</p>
+            </div>
+        )
+    }
+}
---
+++ front-end/src/components/ChartView.js
@@ -0,0 +1,14 @@
+// ChartView.js
+
+import React, { Component } from 'react';
+
+export default class ChartView extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Chart View</h2>
+                <p>Some nice charts will be here</p>
+            </div>
+        )
+    }
+}
