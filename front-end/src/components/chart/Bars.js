4083544daad362ab9a8a85b80d82ce39badb6022
guyit <guyit@github.com>
2018-07-05 18:31:34 -0700

    changes on charts.

--- front-end/src/components/chart/Bars.js
+++ front-end/src/components/chart/Bars.js
@@ -1,18 +1,19 @@
 // Bars.js
 
-import React, { Component } from 'react';
-import {Bar} from 'react-chartjs-2';
-var ApiService = require('../../services/Api').default;
+import React, { Component } from "react";
+import { Bar } from "react-chartjs-2";
+import { compose, withState, withHandlers } from "recompose";
+var ApiService = require("../../services/Api").default;
 var Api = new ApiService();
 
 export default class Bars extends Component {
-
   constructor() {
     super();
     this.state = {
       chartData: {}
-    }
+    };
   }
+
 
   componentWillMount() {
     this.getChartData();
@@ -22,48 +23,46 @@ export default class Bars extends Component {
     //Ajax calls here
     this.setState({
       chartData: {
-        labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
+        labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
         datasets: [
           {
-            label: 'Number of sightings',
-            data: [2,7,8,4],
-            backgroundColor: 'rgb(77,175,165)'
+            label: "Number of sightings",
+            data: [2, 7, 8, 4],
+            backgroundColor: "rgb(77,175,165)"
           }
         ]
       }
     });
   }
 
-    render() {
-        return (
-            <div>
-
-            <Bar
-              data={this.state.chartData}
-              options={{
-                title: {
-                  display: true,
-                  text: 'March 2018',
-                  fontSize: 25
-                },
-                legend: {
-                  display: true,
-                  position: 'top'
-                },
-                  maintainAspectRatio: true,
-                  scales: {
-                    yAxes: [{
-                      ticks: {
-                        beginAtZero: true
-                      }
-                    }]
+  render() {
+    return (
+      <div>
+        <Bar
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: true,
+              text: "March 2018",
+              fontSize: 25
+            },
+            legend: {
+              display: true,
+              position: "top"
+            },
+            maintainAspectRatio: true,
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true
                   }
-              }}
-            />
-
-
-
-            </div>
-        )
-    }
+                }
+              ]
+            }
+          }}
+        />
+      </div>
+    );
+  }
 }
--- front-end/src/components/chart/Top.js
+++ front-end/src/components/chart/Top.js
@@ -1,11 +1,19 @@
 // Top.js
 
-import React, { Component } from 'react';
-import Dropdown from './Dropdown';
+import React, { Component } from "react";
+import Dropdown from "./Dropdown";
 import { compose, withState, withHandlers } from "recompose";
-var ApiService = require('../../services/Api').default;
+var ApiService = require("../../services/Api").default;
 var Api = new ApiService();
 
-export const Top = compose ()(
-  props => <div>{JSON.stringify(props.value)}</div>
-)
+export const Top = compose()(props => (
+  <div>
+    {props.value.map((item, key) => {
+      return <div key={key}>
+        <div><img src={item.photo} /></div>
+        {item.common_name}
+        {item.count}
+      </div>;
+    })}
+  </div>
+));
