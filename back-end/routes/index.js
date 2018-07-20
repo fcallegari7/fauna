afcd95c0e271af28f431a54580e68de0c800dbde
guyit <guyit@github.com>
2018-07-20 08:34:04 -0700

    Added favicon middleware.

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const favicon = require('express-favicon');
+
+const app = express();
+
+app.use(favicon(__dirname + '/public/favicon.png'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
